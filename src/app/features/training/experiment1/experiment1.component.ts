f2480c7a9b281269f5ab6e4dd8c7a59a3920e5e6
owenthcarey <owenthcarey@gmail.com>
2023-07-02 19:14:36 -0700

    Update Android back-pressed event

--- src/app/features/training/experiment1/experiment1.component.ts
+++ src/app/features/training/experiment1/experiment1.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { RouterExtensions } from '@nativescript/angular';
+import {
+  Application,
+  AndroidApplication,
+  AndroidActivityBackPressedEventData,
+} from '@nativescript/core';
 
 @Component({
   moduleId: module.id,
   selector: 'ns-experiment1',
   templateUrl: 'experiment1.component.html',
 })
-export class Experiment1Component {}
+export class Experiment1Component implements OnInit, OnDestroy {
+  constructor(private routerExtensions: RouterExtensions) {}
+
+  ngOnInit() {
+    if (Application.android) {
+      Application.android.on(
+        AndroidApplication.activityBackPressedEvent,
+        (data: AndroidActivityBackPressedEventData) => {
+          // Cancel the default back-button behavior
+          data.cancel = true;
+          // Use RouterExtensions to navigate back
+          this.routerExtensions.back();
+        }
+      );
+    }
+  }
+
+  ngOnDestroy() {
+    if (Application.android) {
+      // Remove the event when the component is destroyed
+      Application.android.off(AndroidApplication.activityBackPressedEvent);
+    }
+  }
+}
