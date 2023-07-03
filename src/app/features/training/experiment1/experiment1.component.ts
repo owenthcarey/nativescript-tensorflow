a28634ae9fa5536eb87eec1395457c10fb050f3c
owenthcarey <owenthcarey@gmail.com>
2023-07-02 19:45:45 -0700

    Add experiment1.service.ts

--- src/app/features/training/experiment1/experiment1.component.ts
+++ src/app/features/training/experiment1/experiment1.component.ts
@@ -5,6 +5,8 @@ import {
   AndroidApplication,
   AndroidActivityBackPressedEventData,
 } from '@nativescript/core';
+import { Canvas } from '@nativescript/canvas';
+import { Experiment1Service } from './experiment1.service';
 
 @Component({
   moduleId: module.id,
@@ -12,7 +14,10 @@ import {
   templateUrl: 'experiment1.component.html',
 })
 export class Experiment1Component implements OnInit, OnDestroy {
-  constructor(private routerExtensions: RouterExtensions) {}
+  constructor(
+    private experiment1Service: Experiment1Service,
+    private routerExtensions: RouterExtensions
+  ) {}
 
   ngOnInit() {
     if (Application.android) {
@@ -33,5 +38,15 @@ export class Experiment1Component implements OnInit, OnDestroy {
       // Remove the event when the component is destroyed
       Application.android.off(AndroidApplication.activityBackPressedEvent);
     }
+  }
+
+  async onCanvasReady(args) {
+    console.log('onCanvasReady()');
+    const canvas = args.object as Canvas;
+    const webGLRenderingContext = canvas.getContext(
+      'webgl2'
+    ) as unknown as WebGLRenderingContext;
+    await this.experiment1Service.init(webGLRenderingContext);
+    this.experiment1Service.multiplyMatrices();
   }
 }
