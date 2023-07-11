c1754ce45010bbb106d8354b0bf4c99087c898f0
owenthcarey <owenthcarey@gmail.com>
2023-07-10 21:39:04 -0700

    Update experiment1.service.ts

--- src/app/features/training/experiment1/experiment1.service.ts
+++ src/app/features/training/experiment1/experiment1.service.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@angular/core';
-import { File, Http, knownFolders, path } from '@nativescript/core';
+import {
+  File,
+  knownFolders,
+  Image,
+  ImageAsset,
+  ImageSource,
+  path,
+} from '@nativescript/core';
 import { Zip } from '@nativescript/zip';
 import {
   Downloader,
@@ -7,6 +14,16 @@ import {
   DownloadEventData,
 } from '@triniwiz/nativescript-downloader';
 import { TensorflowBaseService } from '~/app/core/services/tensorflow.base-service';
+import {
+  Tensor,
+  tensor4d,
+  util,
+  sequential,
+  layers,
+  train,
+  tensor3d,
+  tensor2d,
+} from '@tensorflow/tfjs';
 
 @Injectable({
   providedIn: 'root',
@@ -56,5 +73,97 @@ export class Experiment1Service extends TensorflowBaseService {
       .catch((error) => {
         console.error('Error during unzipping: ', error);
       });
+  }
+
+  async loadAndPreprocessImages() {
+    const tempFolder = knownFolders.temp();
+    const datasetFolder = path.join(tempFolder.path, 'PetImages');
+
+    let dogImages = [];
+    let catImages = [];
+
+    // Here you will need to provide a way to get all the files inside the Dog and Cat folders
+    // This can be achieved using NativeScript's `Folder` and `File` APIs
+
+    // pseudo code
+    // const dogFolder = Folder.fromPath(path.join(datasetFolder, 'Dog'));
+    // dogImages = dogFolder.getEntitiesSync().filter(entity => entity.extension === '.jpg');
+
+    // do the same for catImages
+
+    // Now that we have all the file paths, let's load, decode and preprocess them:
+    const allImages = dogImages.concat(catImages);
+    const allLabels = new Array(dogImages.length)
+      .fill(0)
+      .concat(new Array(catImages.length).fill(1)); // 0 for dog, 1 for cat
+
+    const imageTensors = [];
+    for (let imagePath of allImages) {
+      const imageAsset = new ImageAsset(imagePath);
+      const imageSource = await ImageSource.fromAsset(imageAsset);
+      const imageTensor = tensor3d(imageSource.toTensor());
+      imageTensors.push(imageTensor);
+    }
+
+    // Now we have all our image tensors and labels ready to be converted into a tf.data.Dataset
+    const imageDataset = tensor4d(imageTensors);
+    const labelDataset = tensor2d(allLabels, [allLabels.length, 1]); // make sure the label is a 2D tensor
+    const dataset = util.zipDataset([imageDataset, labelDataset]);
+
+    return dataset;
+  }
+
+  buildModel() {
+    const model = sequential();
+    model.add(layers.rescaling({ scale: 1 / 255, offset: 0 }));
+    model.add(
+      layers.conv2d({
+        inputShape: [180, 180, 3],
+        filters: 32,
+        kernelSize: 3,
+        activation: 'relu',
+      })
+    );
+    model.add(layers.maxPooling2d({ poolSize: 2 }));
+    model.add(
+      layers.conv2d({ filters: 64, kernelSize: 3, activation: 'relu' })
+    );
+    model.add(layers.maxPooling2d({ poolSize: 2 }));
+    model.add(
+      layers.conv2d({ filters: 128, kernelSize: 3, activation: 'relu' })
+    );
+    model.add(layers.maxPooling2d({ poolSize: 2 }));
+    model.add(layers.flatten());
+    model.add(layers.dense({ units: 64, activation: 'relu' }));
+    model.add(layers.dense({ units: 1, activation: 'sigmoid' })); // binary output
+    return model;
+  }
+
+  trainModel(model, dataset) {
+    model.compile({
+      optimizer: train.adam(1e-3),
+      loss: 'binaryCrossentropy',
+      metrics: ['accuracy'],
+    });
+    model.fitDataset(dataset, { epochs: 10, batchSize: 32 });
+  }
+
+  async runInference(model, imagePath) {
+    const imageAsset = new ImageAsset(imagePath);
+    const imageSource = await ImageSource.fromAsset(imageAsset);
+    const imageTensor = tensor3d(imageSource.toTensor());
+    const prediction = model.predict(imageTensor.expandDims(0)) as Tensor; // make sure it's a 4D tensor
+    const score = prediction.dataSync()[0];
+    console.log(
+      `This image is ${(1 - score) * 100}% cat and ${score * 100}% dog.`
+    );
+  }
+
+  async main() {
+    this.downloadAndUnzipDataset();
+    const dataset = await this.loadAndPreprocessImages();
+    const model = this.buildModel();
+    this.trainModel(model, dataset);
+    await this.runInference(model, 'PetImages/Cat/6779.jpg');
   }
 }
